1e71be0d1cf9854f8623806ce2859403aef13972
Grzegorz Warchoł <35039735+grisza24@users.noreply.github.com>
2018-01-08 00:28:14 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace transport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bus = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.inBus = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startStop = new System.Windows.Forms.Button();
+            this.showTab = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bus)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bus
+            // 
+            this.bus.Image = ((System.Drawing.Image)(resources.GetObject("bus.Image")));
+            this.bus.Location = new System.Drawing.Point(13, 3);
+            this.bus.Name = "bus";
+            this.bus.Size = new System.Drawing.Size(100, 43);
+            this.bus.TabIndex = 0;
+            this.bus.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.inBus);
+            this.panel1.Controls.Add(this.bus);
+            this.panel1.Location = new System.Drawing.Point(-1, 194);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(742, 66);
+            this.panel1.TabIndex = 1;
+            // 
+            // inBus
+            // 
+            this.inBus.AutoSize = true;
+            this.inBus.BackColor = System.Drawing.Color.White;
+            this.inBus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.inBus.ForeColor = System.Drawing.Color.DarkRed;
+            this.inBus.Location = new System.Drawing.Point(56, 18);
+            this.inBus.Name = "inBus";
+            this.inBus.Size = new System.Drawing.Size(19, 20);
+            this.inBus.TabIndex = 1;
+            this.inBus.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(58, 132);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(209, 132);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(41, 50);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(382, 132);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(41, 50);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(564, 132);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(41, 50);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(106, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(257, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(430, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(612, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // startStop
+            // 
+            this.startStop.Location = new System.Drawing.Point(13, 13);
+            this.startStop.Name = "startStop";
+            this.startStop.Size = new System.Drawing.Size(99, 37);
+            this.startStop.TabIndex = 10;
+            this.startStop.Text = "Start";
+            this.startStop.UseVisualStyleBackColor = true;
+            this.startStop.Click += new System.EventHandler(this.startStop_Click);
+            // 
+            // showTab
+            // 
+            this.showTab.Location = new System.Drawing.Point(165, 13);
+            this.showTab.Name = "showTab";
+            this.showTab.Size = new System.Drawing.Size(85, 37);
+            this.showTab.TabIndex = 11;
+            this.showTab.Text = "Pokaż tabelę ruchu";
+            this.showTab.UseVisualStyleBackColor = true;
+            this.showTab.Click += new System.EventHandler(this.showTab_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 262);
+            this.Controls.Add(this.showTab);
+            this.Controls.Add(this.startStop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "BusSimulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bus)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bus;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label inBus;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startStop;
+        private System.Windows.Forms.Button showTab;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace transport
+{
+
+    
+    public partial class Form1 : Form
+    {
+        Random gen;//generator liczb losowych
+        TransportDataClassesDataContext db;//obiekt do komunikacji z baza
+        Form Form2;
+        DataGridView table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startStop_Click(object sender, EventArgs e)
+        {
+            if (startStop.Text == "Start")
+            {
+                startStop.Text = "Stop";
+                showTab.Enabled = false;
+                timer1.Start();
+            }
+            else
+            {
+                startStop.Text = "Start";
+                showTab.Enabled = true;
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (bus.Left >= (panel1.Left + panel1.Width))
+            {
+                bus.Left = 0;
+                inBus.Left = bus.Width / 2;
+            }
+            else
+            {
+                bus.Left += 1;
+                inBus.Left += 1;
+            }
+
+            if ((bus.Left + bus.Width / 2) == (pictureBox2.Left + pictureBox2.Width / 2))
+            {
+                int busOut = gen.Next(0,int.Parse(inBus.Text));//ile wysiadlo
+                int busIn = int.Parse(label1.Text);//ile wsiadlo
+
+                startStop_Click(sender, e);
+                inBus.Text = (int.Parse(inBus.Text) - busOut).ToString();
+                inBus.Text = (int.Parse(inBus.Text) + busIn).ToString();
+
+                Thread.Sleep(1500);
+
+                Ruch tmp = new Ruch
+                {
+                    nr_przystanku = 1,
+                    wsiadlo = busIn,
+                    wysiadlo = busOut,
+                    stan_po = int.Parse(inBus.Text),
+                    czas = DateTime.Now
+                };
+                db.Ruches.InsertOnSubmit(tmp);
+                db.SubmitChanges();
+
+                label1.Text = (gen.Next(0, 10)).ToString();
+                startStop_Click(sender, e);
+            }
+
+            if ((bus.Left + bus.Width / 2) == (pictureBox3.Left + pictureBox3.Width / 2))
+            {
+                int busOut = gen.Next(0, int.Parse(inBus.Text));
+                int busIn = int.Parse(label2.Text);
+
+                startStop_Click(sender, e);
+                inBus.Text = (int.Parse(inBus.Text) - busOut).ToString();
+                inBus.Text = (int.Parse(inBus.Text) + busIn).ToString();
+                Thread.Sleep(1500);
+
+                Ruch tmp = new Ruch
+                {
+                    nr_przystanku = 2,
+                    wsiadlo = busIn,
+                    wysiadlo = busOut,
+                    stan_po = int.Parse(inBus.Text),
+                    czas = DateTime.Now.AddMinutes(gen.Next(5,7))
+                };
+                db.Ruches.InsertOnSubmit(tmp);
+                db.SubmitChanges();
+
+                label2.Text = (gen.Next(0, 10)).ToString();
+                startStop_Click(sender, e);
+            }
+
+            if ((bus.Left + bus.Width / 2) == (pictureBox4.Left + pictureBox4.Width / 2))
+            {
+                int busOut = gen.Next(0, int.Parse(inBus.Text));
+                int busIn = int.Parse(label3.Text);
+
+                startStop_Click(sender, e);
+                inBus.Text = (int.Parse(inBus.Text) - busOut).ToString();
+                inBus.Text = (int.Parse(inBus.Text) + busIn).ToString();
+                Thread.Sleep(1500);
+
+                Ruch tmp = new Ruch
+                {
+                    nr_przystanku = 3,
+                    wsiadlo = busIn,
+                    wysiadlo = busOut,
+                    stan_po = int.Parse(inBus.Text),
+                    czas = DateTime.Now.AddMinutes(gen.Next(8, 11))
+                };
+                db.Ruches.InsertOnSubmit(tmp);
+                db.SubmitChanges();
+
+                label3.Text = (gen.Next(0, 10)).ToString();
+                startStop_Click(sender, e);
+            }
+
+            if ((bus.Left + bus.Width / 2) == (pictureBox5.Left + pictureBox5.Width / 2))
+            {
+                int busOut = gen.Next(0, int.Parse(inBus.Text));
+                int busIn = int.Parse(label4.Text);
+
+                startStop_Click(sender, e);
+                inBus.Text = (int.Parse(inBus.Text) - busOut).ToString();
+                inBus.Text = (int.Parse(inBus.Text) + busIn).ToString();
+                Thread.Sleep(1500);
+
+                Ruch tmp = new Ruch
+                {
+                    nr_przystanku = 4,
+                    wsiadlo = busIn,
+                    wysiadlo = busOut,
+                    stan_po = int.Parse(inBus.Text),
+                    czas = DateTime.Now.AddMinutes(gen.Next(12, 18))
+                };
+                db.Ruches.InsertOnSubmit(tmp);
+                db.SubmitChanges();
+
+                label4.Text = (gen.Next(0, 10)).ToString();
+                startStop_Click(sender, e);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gen = new Random();
+            db = new TransportDataClassesDataContext();
+            Form2 = new Form();
+            table = new DataGridView();
+
+            Form2.Size = new Size(670, 300);
+            table.Size = new Size(670, 300);
+            Form2.Controls.Add(table);
+
+            label1.Text = gen.Next(0, 10).ToString();
+            label2.Text = gen.Next(0, 10).ToString();
+            label3.Text = gen.Next(0, 10).ToString();
+            label4.Text = gen.Next(0, 10).ToString();
+        }
+
+        private void showTab_Click(object sender, EventArgs e)
+        {
+            var query = from r in db.Ruches select r;
+
+            table.DataSource = query;
+            Form2.ShowDialog();
+        }
+    }
+}
